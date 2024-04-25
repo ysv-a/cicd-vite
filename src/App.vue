eda7e8ee2c9ec7f958e3b29a45e6dc282211b6cd
Alex <ysov-p@mail.ru>
2024-04-25 16:08:50 +0700

    test push

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="lorem sadfsadsadasdsads" />
-      <HelloWorld msg="lorem sadfsadsadasdsads" />
-      <HelloWorld msg="lorem sadfsadsadasdsads" />
-      <HelloWorld msg="lorem sadfsadsadasdsads" />
-      <HelloWorld msg="lorem sadfsadsadasdsads" />
+      <HelloWorld msg="HELLO WORLD!!!!!!!! 55" />
     </div>
   </header>
 
